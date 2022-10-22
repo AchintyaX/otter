7f0d56bb730621823333b548bd8be53d8fe99ea1
Achintya Shankhdhar <achintya.shankhdhar@simpplr.com>
2022-10-23 01:08:13 +0530

    Update: reformatting fuzzy similarity

--- otter/classification/fuzzy_similarity.py
+++ otter/classification/fuzzy_similarity.py
@@ -14,7 +14,7 @@ class fuzzy_classifier:
         self.taxonomy_dict = taxonomy_dict
         self.inv_map = {v: k for k, v in taxonomy_dict.items()}
         self.taxonomy_arr = [val for key, val in taxonomy_dict.items()]
-    
+
     def text_cleaning(self, text: str) -> str:
         CLEANR = re.compile("<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});")
         text = re.sub(CLEANR, "", text)
@@ -35,14 +35,16 @@ class fuzzy_classifier:
                 "labels_metadata": None,
             }
         result_arr = process.extract(text, taxonomy_arr, limit=3)
-        labels_metadata = [{'label_name': inv_map[i[0]], 'score:': i[1]} for i in result_arr]
+        labels_metadata = [
+            {"label_name": inv_map[i[0]], "score:": i[1]} for i in result_arr
+        ]
 
         return {
             "top_label": labels_metadata[0]["label_name"],
             "top_label_score": labels_metadata[0]["score"],
             "labels_metadata": labels_metadata,
         }
-    
+
     def predict_df(self, df: pd.DataFrame, text_column: str):
         copy_df = df.copy(deep=True)
         logger.info(f"getting predictions from {text_column} column ")
@@ -60,7 +62,7 @@ class fuzzy_classifier:
         copy_df["fuzzy_metadata"] = labels_metadata
 
         return copy_df
-    
+
     def intersection_score(self, df: pd.DataFrame, label_column: str, text_column: str):
 
         output_df = self.predict_df(df=df, text_column=text_column)
